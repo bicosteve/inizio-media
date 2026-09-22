f7d06550e8e3522ade1e2a50d6c6832cb2cf5b73
bsteve <bico.steve@betika.com>
2026-09-22 19:01:56 +0200

    feat(ui): separate css from html and put the css in its own file

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from routes.search import router as search_router
 
 app = FastAPI(title="Google Organic Search Extractor", version="1.0.0")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(search_router)
